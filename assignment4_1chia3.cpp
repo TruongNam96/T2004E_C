c006351b90d359d8373b716ea02157bacb13cb21
TruongNam96 <64963425+TruongNam96@users.noreply.github.com>
2020-05-14 16:59:51 +0700

    assignment4_1

---
+++ assignment4_1chia3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int i,n;
+	printf("nhap mot so tu nhien\n");
+	scanf("%d",&n);
+	
+	for (int i=0;i<n;i++)
+		if (i%3==0){
+		printf ("so %d chia het cho 3 \n",i);
+}
+}
+
