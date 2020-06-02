32d68d0ab9c9415fe743b9e80750545b2bbd9901
TruongNam96 <truonghainam26121996@gmail.com>
2020-06-02 19:57:38 +0700

    practical

---
+++ practical_1.cpp
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+
+int main()
+{
+    char str1[50], str2[50];
+    int i, j;
+
+    printf("string 1: ");
+    gets(str1);
+    printf("string 2: ");
+    gets(str2);
+
+
+    i=0;
+    while(str1[i] != '\0')
+    {
+        i++;
+    }
+
+    j = 0;
+    while(str2[j] != '\0')
+    {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
+
+    printf("Concatenated string = %s", str1);
+
+    return 0;
+}
